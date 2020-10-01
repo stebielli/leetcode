c0638cc1d04e9221d0de387581c15f5107969861
Stefano Bielli <stefano.bielli@gmail.com>
2020-10-01 11:56:20 +0200

    Exercise2

---
+++ src/main/java/it/bielli/leetcode/Exercise2.java
@@ -0,0 +1,78 @@
+package it.bielli.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/zigzag-conversion/
+ */
+public class Exercise2 {
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return  s;
+        }
+
+        List<LinkedList<Character>> ds = initDataStructure(numRows);
+
+        boolean forward = true;
+        int verticalPosition = 0;
+        for (int index = 0; index < s.length(); index++) {
+            if (!forward) {
+                nullifyOthers(ds, verticalPosition);
+            }
+
+            ds.get(verticalPosition).addLast(s.charAt(index));
+
+            if (forward) {
+                if (verticalPosition + 1 < numRows) {
+                    verticalPosition++;
+                } else {
+                    forward = false;
+                    verticalPosition--;
+                }
+            } else {
+                if (verticalPosition - 1 >= 0) {
+                    verticalPosition--;
+                } else {
+                    forward = true;
+                    verticalPosition++;
+                }
+            }
+        }
+
+        return convert(ds);
+    }
+
+    private List<LinkedList<Character>> initDataStructure(int numRows) {
+        List<LinkedList<Character>> ds = new ArrayList<>(numRows);
+        for (int i = 0; i < numRows; i++) {
+            ds.add(new LinkedList<>());
+        }
+        return ds;
+    }
+
+    private void nullifyOthers(List<LinkedList<Character>> ds, int verticalPosition) {
+        for (int i = 0; i < ds.size(); i++) {
+            if (i != verticalPosition) {
+                ds.get(i).addLast(null);
+            }
+        }
+    }
+
+    private String convert(List<LinkedList<Character>> ds) {
+        StringBuilder conversion = new StringBuilder();
+
+        for (LinkedList<Character> list : ds) {
+            while (!list.isEmpty()) {
+                Character c = list.pollFirst();
+                if (c != null) {
+                    conversion.append(c);
+                }
+            }
+        }
+        return conversion.toString();
+    }
+
+}
---
+++ src/test/java/it/bielli/leetcode/Exercise2Test.java
@@ -0,0 +1,39 @@
+package it.bielli.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * https://leetcode.com/problems/zigzag-conversion/
+ */
+public class Exercise2Test {
+
+    private Exercise2 ex;
+
+    @BeforeEach
+    void setUp() {
+        ex = new Exercise2();
+    }
+
+    @Test
+    void test1() {
+        assertEquals("A", ex.convert("A", 1));
+    }
+
+    @Test
+    void test2() {
+        assertEquals("PAHNAPLSIIGYIR", ex.convert("PAYPALISHIRING", 3));
+    }
+
+    @Test
+    void test3() {
+        assertEquals("PINALSIGYAHRPI", ex.convert("PAYPALISHIRING", 4));
+    }
+
+    @Test
+    void test4() {
+        assertEquals("AB", ex.convert("AB", 1));
+    }
+}
